0455a75bd4b9fe6c7b0ac4b3c3cd8f5f83a47fdb
jacam666 <ja6cam@gmail.com>
2021-06-27 19:07:38 +0100

    Add project files.

---
+++ using else/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace using_else
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double payment = Convert.ToInt32(Console.ReadLine());
+            double balance = Convert.ToInt32(Console.ReadLine());
+
+            if (balance >= payment)
+            {
+                Console.WriteLine("Completed");
+            }
+            else
+            {
+                Console.WriteLine("Insufficient funds");
+            }
+        }
+    }
+}
