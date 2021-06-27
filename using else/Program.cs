4228657744a7ec4b8ad40a2c36b85a685170e484
jacam666 <ja6cam@gmail.com>
2021-06-27 19:10:04 +0100

    added writelines

--- using else/Program.cs
+++ using else/Program.cs
@@ -6,7 +6,9 @@ namespace using_else
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Total bill is :");
             double payment = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Total balance is :");
             double balance = Convert.ToInt32(Console.ReadLine());
 
             if (balance >= payment)
